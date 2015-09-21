a93f4bee0378ef7c6cf373570ba2fc1e3c20f66d
Sqrt of Saturn <sqrt@octoblu.com>
2015-09-21 14:37:22 -0700

    v3.1.0 upgraded connector

--- command.js
+++ command.js
@@ -3,8 +3,9 @@ var config = require('./meshblu.json');
 var Connector = require('./connector');
 
 var connector = new Connector(config);
-connector.run();
 
-connector.on('error', function(error){
+connector.on('error', function(error) {
   console.error(error);
 });
+
+connector.run();
